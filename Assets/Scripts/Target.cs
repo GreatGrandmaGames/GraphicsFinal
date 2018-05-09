e17fd61c2b2482a8bc1f64789f58467a3067330f
Nina Demirjian <nsd286@nyu.edu>
2018-05-09 19:03:06 -0400

    target b updated

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class Target : MonoBehaviour {
 
+	public float scoreToAdd; //varies depending on which level of ring this script is attached to
+
 	void OnCollisionEnter (Collision col)
 	{
-		if(col.gameObject.name == "arrow")
+		if(col.gameObject.name == "arrow") // when the arrow collides
 		{
 			Debug.Log ("you hit a target!");
-			Scoring.playerScore += 10f;
+			Scoring.playerScore += scoreToAdd;
 		}
 	}
 
